3fe1f01b5e4c3745b28eac3f7f5f63e351c608ca
Alessandro Pignanelli <alep5@outlook.it>
2025-07-13 23:42:31 +0200

    test(sweep): add polygon sweep test

--- tests/geometry/test_SweepRaycast_performance.cpp
+++ tests/geometry/test_SweepRaycast_performance.cpp
@@ -20,7 +20,7 @@ TEST_CASE("Sweep raycast performance: Circle", "[Sweep][Benchmark][Raycast]")
     auto translations = generateRandomTranslations(100'000, -5.0f, 5.0f, seed + 1);
     auto rays = generateRandomRays(100'000, -60.0f, 60.0f, seed + 2);
 
-    BENCHMARK_FUNCTION("Sweep raycast 10k circles", 2ms, [&]()
+    BENCHMARK_FUNCTION("Sweep raycast 10k circles", 5ms, [&]()
     {
         uint32_t total = 0;
         for (uint32_t i = 0; i < 10'000; ++i)
@@ -31,12 +31,43 @@ TEST_CASE("Sweep raycast performance: Circle", "[Sweep][Benchmark][Raycast]")
         return total;
     });
 
-    BENCHMARK_FUNCTION("Sweep raycast 100k circles", 15ms, [&]()
+    BENCHMARK_FUNCTION("Sweep raycast 100k circles", 50ms, [&]()
     {
         uint32_t total = 0;
         for (uint32_t i = 0; i < 100'000; ++i)
         {
             if (sweep(circles[i], Transform{}, Transform{translations[i], 0}, rays[i]))
+                ++total;
+        }
+        return total;
+    });
+}
+
+TEST_CASE("Sweep raycast performance: Polygon", "[SweepRaycast][Benchmark][Polygon]")
+{
+    const auto seed = Catch::getCurrentContext().getConfig()->rngSeed();
+    microseconds elapsed;
+    auto polygons = generateRandomRectangles(100'000, -50.0f, 50.0f, 0.5f, seed);
+    auto rays = generateRandomRays(100'000, -60.0f, 60.0f, seed + 1);
+    auto translations = generateRandomTranslations(100'000, -5.0f, 5.0f, seed + 2);
+
+    BENCHMARK_FUNCTION("Sweep raycast 10k polygons", 10ms, [&]()
+    {
+        uint32_t total = 0;
+        for (uint32_t i = 0; i < 10'000; ++i)
+        {
+            if (sweep(polygons[i], Transform{}, Transform{translations[i], 0}, rays[i]))
+                ++total;
+        }
+        return total;
+    });
+
+    BENCHMARK_FUNCTION("Sweep raycast 100k polygons", 80ms, [&]()
+    {
+        uint32_t total = 0;
+        for (uint32_t i = 0; i < 100'000; ++i)
+        {
+            if (sweep(polygons[i], Transform{}, Transform{translations[i], 0}, rays[i]))
                 ++total;
         }
         return total;
